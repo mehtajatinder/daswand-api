0406f3796764786a9d07189fa4b990a0503acef1
nipun <nipunkuchroo2007@gmail.com>
2021-02-17 02:52:34 +0530

    login and register changes

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -20,73 +20,66 @@ exports.register = (req, res, next) => {
     })
     .then((data) => {
       if (data[0][0][0].userID == 0) {
-        res.status(400).json({
-          token: tempToken,
-          message: data[0][0][0].message,
-        });
+        const error = new Error("user not created");
+        error.statusCode = 400;
+        throw error;
       } else {
-        const tempToken = jwt.sign(
-          { userID: data[0][0][0].userID },
-          "daswands",
-          {
-            expiresIn: "1h",
-          }
-        );
         res.status(200).json({
-          token: tempToken,
           message: data[0][0][0].message,
         });
       }
     })
-    .catch((err) => {
-      console.log(1);
+    .catch((error) => {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+        error.message = "error occured.";
+      }
+      res.status(error.statusCode).json({
+        message: error.message,
+      });
     });
-
-  // db.execute("call registerUser(?,?,?,?)", [
-  //   firstname,
-  //   lastname,
-  //   email,
-  //   password,
-  // ])
-  //   .then((data) => {
-  //     //  console.log(data)
-  //     if (data[0][0][0].userID == 0) {
-  //       res.status(400).json({
-  //         token: tempToken,
-  //         message: data[0][0][0].message,
-  //       });
-  //     } else {
-  //       const tempToken = jwt.sign(
-  //         { userID: data[0][0][0].userID },
-  //         "daswand",
-  //         {
-  //           expiresIn: "1h",
-  //         }
-  //       );
-  //       res.status(200).json({
-  //         token: tempToken,
-  //         message: data[0][0][0].message,
-  //       });
-  //     }
-  //   })
-  //   .catch((error) => {
-  //     res.status(500).json({
-  //       error: error,
-  //     });
-  //   });
 };
 
 exports.login = (req, res, next) => {
-  db.execute("call loginUser(?,?)", [req.body.username, req.body.password])
+  const password = req.body.password;
+  db.execute("call loginUser(?)", [req.body.username])
     .then((data) => {
-      console.log(data);
-      res.status(200).json({
-        message: "user logged in",
-      });
+      if (data[0][0][0].userID == 0) {
+        const error = new Error("user does not exists");
+        error.statusCode = 400;
+        throw error;
+      } else {
+        data.userID = data[0][0][0].userID;
+        data.bcryptResult = bcrypt.compareSync(
+          password,
+          data[0][0][0].userPassword
+        );
+        return data;
+      }
+    })
+    .then((data) => {
+      if (data.bcryptResult == true) {
+        const tempToken = jwt.sign({ userID: data.userID }, "daswands", {
+          expiresIn: "1h",
+        });
+        res.status(200).json({
+          token: tempToken,
+          userId: data.userID,
+          message: "user logged in",
+        });
+      } else {
+        const error = new Error("incorrect username/password.");
+        error.statusCode = 400;
+        throw error;
+      }
     })
     .catch((error) => {
-      res.status(500).json({
-        message: "invalid username password. tattti",
+      if (!error.statusCode) {
+        error.statusCode = 500;
+        error.message = "error occured";
+      }
+      res.status(error.statusCode).json({
+        message: error.message,
       });
     });
 };
